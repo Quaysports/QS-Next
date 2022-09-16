577a40da8dbf6de0863cca139489dc73ddab09a1
Geoff <geoff.petrides@gmail.com>
2022-09-16 12:01:52 +0100

    Update: Fixed nested order not deleting

--- pages/api/shop-orders/delete-order.ts
+++ pages/api/shop-orders/delete-order.ts
@@ -29,7 +29,7 @@ interface orderItem {
 export default async function handler(req, res) {
     console.log(req.body)
     res.status(200).json(
-        await deleteOrder(req.body)
+        await deleteOrder(req.body.order)
     )
 }
 
