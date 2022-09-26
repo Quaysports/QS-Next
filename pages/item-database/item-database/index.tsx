fbed5b224dc8ccd5b9b3bcf5fe5c075476ce7db0
Richard Devine <richard_devine@outlook.com>
2022-09-26 10:56:25 +0100

    rod locations completed and global css tweaks

--- pages/item-database/item-database/index.tsx
+++ pages/item-database/item-database/index.tsx
@@ -1,7 +1,7 @@
 import styles from "../item-database.module.css";
 import SearchBar from "../../../components/search-bar";
 
-export default function ItemDatabaseLandingPage(){
+export default function ItemDatabaseLandingPage() {
 
     const item = "svc"
 
@@ -9,20 +9,34 @@ export default function ItemDatabaseLandingPage(){
 
     }
 
-    function buildSideMenu(){
-        if(item){
+    function buildSideMenu() {
+        if (item) {
             return [
-                <div className={styles["sideBar"]}>
-                    <div>
-                        <div><button>Barcode</button></div>
-                        <div><button>Tag</button></div>
-                        <div><button>Shelf Tag</button></div>
-                        <div style={{margin: "20px 0"}}><button>Upload to Linnworks</button></div>
-                        <div><button>Jarilo Template</button></div>
-                        <div><button>Import Details</button></div>
-                        <div><button>Branded Labels</button></div>
+                    <div className={styles["side-bar"]}>
+                        <span className={styles["side-bar-cell"]}>
+                            <div>
+                                <button>Barcode</button>
+                            </div>
+                            <div>
+                                <button>Tag</button>
+                            </div>
+                            <div>
+                                <button>Shelf Tag</button>
+                            </div>
+                            <div style={{margin: "20px 0"}}>
+                                <button style={{height: "35px"}}>Upload to Linnworks</button>
+                            </div>
+                            <div>
+                                <button>Jarilo Template</button>
+                            </div>
+                            <div>
+                                <button>Import Details</button>
+                            </div>
+                            <div>
+                                <button>Branded Labels</button>
+                            </div>
+                        </span>
                     </div>
-                </div>
             ]
         }
     }
@@ -30,9 +44,9 @@ export default function ItemDatabaseLandingPage(){
     return (
         <>
             <div className={styles["search-bar-container"]}><SearchBar itemIndex={x => searchOptions(x)}
-                                                                        searchableArray={[]} EAN={false}/></div>
-                {buildSideMenu()}
-                <div className={styles["table"]}></div>
+                                                                       searchableArray={[]} EAN={false}/></div>
+            {item ? buildSideMenu() : null}
+            {item ? <div className={styles["table"]}></div> : null}
         </>
     )
 }
--- pages/item-database/rod-locations/index.tsx
+++ pages/item-database/rod-locations/index.tsx
@@ -1,16 +1,69 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import styles from "../item-database.module.css"
+
+interface rodLocationObject {
+    BRANDLABEL:{loc:string},
+    IDBEP:{BRAND:string},
+    SKU:string,
+    TITLE:string,
+    _id: string
+}
 
 export default function RodLocationsLandingPage(){
+
+    const [rodLocations, setRodLocations] = useState<Map<string, rodLocationObject[]>>(new Map())
+    const [brandFilter, setBrandFilter] = useState<string>(null)
 
     useEffect(() => {
     fetch("/api/item-database/rod-locations")
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => {
+            let sortedData = res.sort((a, b) => {
+                if (!a.IDBEP.BRAND) a.IDBEP.BRAND = "Default"
+                if (!b.IDBEP.BRAND) b.IDBEP.BRAND = "Default"
+                return a.IDBEP.BRAND.localeCompare(b.IDBEP.BRAND)
+            })
+            let tempMap = new Map()
+            sortedData.map((item) => {
+                tempMap.has(item.IDBEP.BRAND) ?
+                    tempMap.get(item.IDBEP.BRAND).push(item) : tempMap.set(item.IDBEP.BRAND, [item])
+            })
+          setRodLocations(tempMap)
+        })
+
     },[])
 
-    return(
-        <div>
+    function buildFilter(){
+        let tempArray = []
+        rodLocations.forEach((item, key) => {
+            tempArray.push(<button onClick={() => setBrandFilter(key)}>{key} ({item.length})</button>)
+        })
+        return <span className={styles["side-bar-cell"]}>{tempArray}</span>
+    }
 
+    function buildTable() {
+        let tempArray = [<div className={styles["rod-location-title"]}><span/><span>SKU</span><span>Title</span><span className={"center-align"}>Location</span></div>]
+        rodLocations.get(brandFilter).map((item) => {
+            tempArray.push(<div className={styles["rod-location-grid"]}>
+                <span/>
+                <span>{item.SKU}</span>
+                <span>{item.TITLE}</span>
+                <span className={"center-align"}>{item.BRANDLABEL.loc}</span>
+            </div>)
+        })
+        return <>{tempArray}</>
+    }
+
+
+    return(
+        <>
+        <div className={styles["side-bar"]}>
+            {rodLocations ? "Suppliers" : null}
+            {rodLocations ? buildFilter() : null}
         </div>
+        <div className={styles["table"]}>
+            {brandFilter ? buildTable() : null}
+        </div>
+        </>
     )
 }
