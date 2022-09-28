ecf141338597c486c1f920f22e706bb5dbfe67f4
Geoff <geoff.petrides@gmail.com>
2022-09-28 10:05:49 +0100

    Updated: Change var name to be more descriptive

--- components/layouts/column-layout.tsx
+++ components/layouts/column-layout.tsx
@@ -2,21 +2,21 @@ import styles from './layout-styles.module.css'
 
 export default function ColumnLayout({children, scroll = false, maxWidth = undefined}){
 
-    const cssStyles = {overflowY:null, margin:null, maxHeight:null, maxWidth:null}
+    const styleOverrides = {overflowY:null, margin:null, maxHeight:null, maxWidth:null}
 
     if(scroll){
-        cssStyles.overflowY = "scroll"
-        cssStyles.maxHeight = "calc(100vh - 52px)"
+        styleOverrides.overflowY = "scroll"
+        styleOverrides.maxHeight = "calc(100vh - 52px)"
     }
     if(maxWidth){
-        cssStyles.margin = "var(--sidebar-margin) auto"
-        cssStyles.maxWidth = maxWidth
+        styleOverrides.margin = "var(--sidebar-margin) auto"
+        styleOverrides.maxWidth = maxWidth
     }
 
     return(
         <div className={styles["column-layout"]}>
             <div
-                style={cssStyles}
+                style={styleOverrides}
             >{children}</div>
         </div>
     )
