7f5c535d007a98c2743b2df084e24fc9f528702b
Geoff <geoff.petrides@gmail.com>
2022-11-16 11:42:31 +0000

    Updated: Fixed Linnworks update all fetch

--- pages/api/linnworks/update-channel-prices.ts
+++ pages/api/linnworks/update-channel-prices.ts
@@ -2,16 +2,21 @@ import {NextApiRequest, NextApiResponse} from "next";
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
-    const opts = {
+    interface Options{
+        method:string
+        headers: {[key:string]:string}
+        body?:string
+    }
+
+    const opts:Options = {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
             "token": "9b9983e5-30ae-4581-bdc1-3050f8ae91cc"
-        },
-        body: JSON.stringify(req.body)
+        }
     }
 
-    console.dir(opts)
+    if(req.body) opts.body = JSON.stringify(req.body)
 
     let result = await fetch("http://localhost:3001/Linn/UpdateLinnChannelPrices", opts)
     res.status(200).json(await result.json())
