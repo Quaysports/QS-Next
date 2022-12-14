3813da8f999d493029faa59f9dbd24faeb9d9b9b
Geoff <geoff.petrides@gmail.com>
2022-12-14 15:22:52 +0000

    Updated: Removed unused tabs

--- pages/dashboard/tabs.tsx
+++ pages/dashboard/tabs.tsx
@@ -25,12 +25,15 @@ export default function DashboardTabs() {
         <>
             <span className={activeTab("home")} data-testid={"home-tab"}><Link href="/dashboard?tab=home">Home</Link></span>
             {permissions?.users?.auth ? <span className={activeTab("user")} data-testid={"user-tab"}><Link href="/dashboard?tab=user">Users</Link></span> : null}
+            {permissions?.holidays?.auth ? <span className={activeTab("holidays")}><Link href="/dashboard?tab=holidays">Holidays</Link></span> : null}
+        </>
+    )
+}
+
+/* ToDo tabs:
             {permissions?.orderSearch?.auth ? <span className={activeTab("order-search")} data-testid={"orders-search-tab"}><Link href="/dashboard?tab=order-search">Order Search</Link></span> : null}
             {permissions?.priceUpdates?.auth ? <span className={activeTab("price-updates")}><Link href="/dashboard?tab=price-updates">Price Updates</Link></span> : null}
             {permissions?.shop?.auth ? <span className={activeTab("shop")}><Link href="/dashboard?tab=shop">Shop</Link></span> : null}
             {permissions?.online?.auth ? <span className={activeTab("online")}><Link href="/dashboard?tab=online">Online</Link></span> : null}
             {permissions?.rotas?.auth ? <span className={activeTab("rotas")}><Link href="/dashboard?tab=rotas">Rotas</Link></span> : null}
-            {permissions?.holidays?.auth ? <span className={activeTab("holidays")}><Link href="/dashboard?tab=holidays">Holidays</Link></span> : null}
-        </>
-    )
-}
+ */
