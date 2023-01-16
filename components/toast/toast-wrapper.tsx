7627596c410237389514ec098a35671980d67f0b
Geoff <geoff.petrides@gmail.com>
2023-01-16 15:45:32 +0000

    Added: Breakfast is served

---
+++ components/toast/toast-wrapper.tsx
@@ -0,0 +1,45 @@
+import Toast, {Options} from "./toast";
+import {useEffect, useRef, useState} from "react";
+import {guid} from "../../server-modules/core/core";
+
+/**
+ * A wrapper component that listens for a custom event called "notification" and when it receives the event, it passes the event's detail to the Notification component
+ */
+export default function ToastWrapper() {
+
+    const element = useRef<HTMLDivElement>(null)
+
+    const [toastContent, setToastContent] = useState<Options & {key:string}| undefined>()
+    const [toastRender, setToastRender] = useState<{ [key: string]: { duration: number, options: Options } }>({})
+
+
+    useEffect(() => {
+        element.current?.addEventListener('toast', toastEventHandler);
+        return () => {
+            element.current?.removeEventListener('toast', toastEventHandler);
+        };
+    }, [])
+
+    useEffect(() => {
+        if (Object.keys(toastRender).length === 0) return
+        const key = Object.keys(toastRender)[0]
+        setToastContent({...toastRender[key].options, key: key})
+    }, [toastRender])
+
+    function toastAnimationHandler(key:string){
+        delete toastRender[key]
+        setToastRender({...toastRender})
+    }
+
+    function toastEventHandler(e: Event) {
+        setToastRender(msg => ({
+                ...msg,
+                ...{[guid()]: {duration: 5, options: (e as CustomEvent).detail as Options}}
+            })
+        )
+    }
+
+    return <div id="toast-target" ref={element}>
+        <Toast key={toastContent?.key} options={toastContent} handler={toastAnimationHandler} />
+    </div>
+}
