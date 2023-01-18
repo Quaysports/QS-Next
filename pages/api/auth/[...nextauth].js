21efd89099ce15e4c53d7a276d0454f29cbf20a7
Geoff <geoff.petrides@gmail.com>
2023-01-18 11:23:31 +0000

    Updated: Tweaks to login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import {login, pinLogin} from '../../../server-modules/users/user'
 
 export const authOptions = {
+    site: process.env.NEXTAUTH_URL,
     providers: [
         CredentialsProvider({
             id: 'credentials',
