5122d093a762e23a42123193be1218cabe53c957
Richard Devine <richard_devine@outlook.com>
2023-01-18 08:42:50 +0000

    fixed shop order tool

--- pages/shop-orders/orders/submit-to-linnworks-buttons.tsx
+++ pages/shop-orders/orders/submit-to-linnworks-buttons.tsx
@@ -24,11 +24,13 @@ export default function SubmitToLinnworksButtons() {
 
     async function submitToLinnworks() {
         let data = []
+        console.log(loadedOrder)
         for (let item of loadedOrder!.arrived) {
             if (item.quantity > 0 && !item.submitted && !item.newProduct) {
-                data.push({SKU: item.SKU, QTY: item.tradePack * item.quantity})
+                data.push({SKU: item.SKU, quantity: item.tradePack * item.quantity})
             }
         }
+        console.log(data)
 
         if (data.length > 0) {
             const opts = {
