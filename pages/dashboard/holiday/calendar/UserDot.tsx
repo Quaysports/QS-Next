b97cb7428d8f3dcebd1f7adf73f36f3f6e3a4e7c
Geoff <geoff.petrides@gmail.com>
2023-01-20 10:23:42 +0000

    Updated: Added guard statement

--- pages/dashboard/holiday/calendar/UserDot.tsx
+++ pages/dashboard/holiday/calendar/UserDot.tsx
@@ -2,19 +2,21 @@ import styles from "./calendar.module.css";
 import {useSelector} from "react-redux";
 import {selectUserColors} from "../../../../store/dashboard/holiday-slice";
 
-export default function UserDot({user, booked}:{user: string, booked: sbt.HolidayOrSickBooking}){
+export default function UserDot({user, booked}: { user: string, booked: sbt.HolidayOrSickBooking }) {
     const userColors = useSelector(selectUserColors)
-    if(booked.type === "holiday") {
-        switch (booked.duration) {
-            case 50:
-                return <div className={styles["half-booked-dot"]}
-                            style={{backgroundColor: userColors[user]}}/>
-            case 100:
-                return <div className={styles["booked-dot"]}
-                            style={{backgroundColor: userColors[user]}}/>
-            default:
-                return null
-        }
+
+    if(!booked || !user) return null
+
+    if (booked.type !== "holiday") return null
+
+    switch (booked.duration) {
+        case 50:
+            return <div className={styles["half-booked-dot"]}
+                        style={{backgroundColor: userColors[user]}}/>
+        case 100:
+            return <div className={styles["booked-dot"]}
+                        style={{backgroundColor: userColors[user]}}/>
+        default:
+            return null
     }
-    return null
 }
