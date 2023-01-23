70cbb055af2d49fadc9189157de79bd5f75dde13
Richard Devine <richard_devine@outlook.com>
2023-01-23 14:33:03 +0000

    upload to linnworks button updated

--- pages/api/item-database/delete-image.ts
+++ pages/api/item-database/delete-image.ts
@@ -1,7 +1,6 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse){
-    console.log(req.body)
     const opt = {
         method: 'POST',
         headers: {
@@ -11,6 +10,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse){
         body:JSON.stringify(req.body)
     }
 
+    
     const result = await fetch('http://localhost:4000/Items/DeleteImage', opt)
 
     res.send(result)
