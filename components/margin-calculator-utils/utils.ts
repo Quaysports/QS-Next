1c31fcc28b2f68dc58f024b4cfd863d4daae75a0
Geoff <geoff.petrides@gmail.com>
2023-01-27 07:56:48 +0000

    Updated: Forces currency display to 2dp

--- components/margin-calculator-utils/utils.ts
+++ components/margin-calculator-utils/utils.ts
@@ -1,5 +1,5 @@
 export function toCurrency(value?:number){
-    return value ? `£${value.toLocaleString("en-GB", {minimumFractionDigits: 2})}` : "£0.00"
+    return value ? `£${value.toLocaleString("en-GB", {minimumFractionDigits: 2, maximumFractionDigits:2})}` : "£0.00"
 }
 
 export function toTitleCase(string?:string){
