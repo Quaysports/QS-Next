e644810c6eb80bf250861b4a40a15d0ea06a9e8a
Geoff <geoff.petrides@gmail.com>
2023-02-06 09:12:00 +0000

    Dev conversions script for quicklinks

---
+++ pages/api/dev/convert-quicklinks.ts
@@ -0,0 +1,6 @@
+import {convertQuickLinks} from "../../../server-modules/dev/dev";
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    res.status(200).json(await convertQuickLinks())
+}
