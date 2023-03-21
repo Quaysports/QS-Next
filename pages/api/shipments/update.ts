d32e57e42639a381f610515d6624eae9fd0bd3ee
Geoff <geoff.petrides@gmail.com>
2023-03-21 11:24:58 +0000

    Tweaked shipping update

--- pages/api/shipments/update.ts
+++ pages/api/shipments/update.ts
@@ -1,6 +1,23 @@
-import {updateShipment} from "../../../server-modules/shipping/shipping";
+import {Shipment, ShipmentItem, updateShipment} from "../../../server-modules/shipping/shipping";
 import {NextApiRequest, NextApiResponse} from "next";
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    res.status(200).json(await updateShipment(req.body))
+    let shipment:Shipment = req.body
+    let data = await updateShipment(shipment)
+    let opts = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "token": "9b9983e5-30ae-4581-bdc1-3050f8ae91cc"
+        },
+        body: JSON.stringify({skuList: skuList(shipment.data)})
+    }
+    await fetch("http://localhost:4000/Items/UpdateItemStock", opts)
+    res.status(200).json(data)
+}
+
+const skuList = (data:ShipmentItem[]) => {
+    return data.reduce((str, item) => {
+        return str === '' ? `'${item.sku}'` : str + `,'${item.sku}'`
+    }, "")
 }
