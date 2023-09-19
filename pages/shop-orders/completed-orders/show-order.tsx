05236a5db67b4adbe16e9a9de124a235c97e1599
Mrmattbaker <139124601+Mrmattbaker@users.noreply.github.com>
2023-09-19 10:28:51 +0100

    Added TP size to Completed orders
    Needs checking with Staff that run completed orders, and potential to add another field for raw arrival amount

--- pages/shop-orders/completed-orders/show-order.tsx
+++ pages/shop-orders/completed-orders/show-order.tsx
@@ -1,36 +1,49 @@
-import styles from "../shop-orders.module.css";
-import {useSelector} from "react-redux";
-import {selectOrderContents} from "../../../store/shop-orders-slice";
+import React from 'react';
+import styles from '../shop-orders.module.css';
+import { useSelector } from 'react-redux';
+import { selectOrderContents } from '../../../store/shop-orders-slice';
+
+interface OrderItem {
+  quantity: number;
+  tradePack?: number;
+  SKU: string;
+  title: string;
+}
 
 export default function ShowOrder() {
+  const orderContents = useSelector(selectOrderContents);
 
-    const orderContents = useSelector(selectOrderContents)
+  if (orderContents) {
+    const tempArray: JSX.Element[] = [];
+    orderContents.arrived.forEach((item: OrderItem, index: number) => {
+      tempArray.push(
+        <div
+          key={index}
+          className={`${styles['shop-orders-table']} ${styles['shop-orders-table-cells']} ${styles['completed-orders-list-grid']}`}
+        >
+          <span className={'center-align'}>{item.quantity * (item.tradePack ? item.tradePack : 1)}</span>
+          <span>{item.tradePack === null || item.tradePack === undefined || item.tradePack === 1 ? 'N/a' : item.tradePack}</span>
+          <span>{item.SKU}</span>
+          <span>{item.title}</span>
+        </div>
+      );
+    });
 
-    if (orderContents) {
-        let tempArray:JSX.Element[] = []
-        orderContents.arrived.forEach((item, index) => {
-                tempArray.push(
-                    <div key={index}
-                         className={`${styles["shop-orders-table"]} ${styles["shop-orders-table-cells"]} ${styles["completed-orders-list-grid"]}`}>
-                        <span className={"center-align"}>{item.quantity * (item.tradePack ? item.tradePack : 1)}</span>
-                        <span>{item.SKU}</span>
-                        <span>{item.title}</span>
-                    </div>
-                )
-            }
-        )
-        return (
-            <div className={styles["shop-orders-table-containers"]}>
-                {orderContents.completedBy !== "unknown" && <div className={styles["shop-orders-table-completed-by"]}>{`Completed by: ${orderContents.completedBy}`}</div>}
-                <div className={`${styles["shop-orders-table"]} ${styles["completed-orders-list-grid"]}`}>
-                    <span className={"center-align"}>Amount</span>
-                    <span>SKU</span>
-                    <span>Title</span>
-                </div>
-                {tempArray}
-            </div>
-        )
-    } else {
-        return null
-    }
+    return (
+      <div className={styles['shop-orders-table-containers']}>
+        {orderContents.completedBy !== 'unknown' && (
+          <div className={styles['shop-orders-table-completed-by']}>{`Completed by: ${orderContents.completedBy}`}</div>
+        )}
+        <div className={`${styles['shop-orders-table']} ${styles['completed-orders-list-grid']}`}>
+          <span className={'center-align'}>Amount</span>
+          <span>T/P</span>
+          <span>SKU</span>
+          <span>Title</span>
+        </div>
+        {tempArray}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
