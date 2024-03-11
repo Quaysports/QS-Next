e84a65b0bc17d9f03e0877cf71bcf1f86f35fb5f
numberwang55 <cban0901@hotmail.com>
2024-03-11 13:53:38 +0000

    Update: Mongo find function to sort A-Z

--- server-modules/mongo-interface/mongo-interface.ts
+++ server-modules/mongo-interface/mongo-interface.ts
@@ -144,37 +144,31 @@ export const bulkUpdateAny = async (collection: string, arr: mongoDB.Document[],
 //   }
 // }
 
-// new find function implementation to allow for extended properties filter in projection
 export const find = async <T>(collection: string, filter = {}, projection = {}, sort = {}, limit = 20000) => {
-  const client = await connect();
-  try {
-      const db = client.db(process.env.DB_NAME);
-      const cursor = db.collection(collection).find(filter).project(projection).limit(limit);
+    const client = await connect();
+    try {
+        const db = client.db(process.env.DB_NAME);
+        const data = await db.collection(collection).find(filter).sort(sort).toArray() as T[];
+        
+        // Filter and transform the documents based on the projection
+        const transformedData = data.map((doc: any) => {
+            // Check if the document has the extendedProperties field
+            if (doc.extendedProperties) {
+                // Filter the extendedProperties array to include only elements with epName equal to "Special Price"
+                const specialPriceProps = doc.extendedProperties.filter((prop: any) => prop.epName === "Special Price");
+                // Assign the filtered array back to the extendedProperties field
+                doc.extendedProperties = specialPriceProps;
+            }
+            return doc;
+        });
 
-      // Apply sorting
-      cursor.sort(sort);
-
-      const data = await cursor.toArray() as T[];
-
-      // Filter and transform the documents based on the projection
-      const transformedData = data.map((doc: any) => {
-          // Check if the document has the extendedProperties field
-          if (doc.extendedProperties) {
-              // Filter the extendedProperties array to include only elements with epName equal to "Special Price"
-              const specialPriceProps = doc.extendedProperties.filter((prop: any) => prop.epName === "Special Price");
-              // Assign the filtered array back to the extendedProperties field
-              doc.extendedProperties = specialPriceProps;
-          }
-          return doc;
-      });
-
-      return convertBSONIDsToString<T[]>(transformedData);
-  } catch (e) {
-      console.error(e);
-      throw e; // Re-throw the error to handle it elsewhere
-  } finally {
-      await client.close();
-  }
+        return convertBSONIDsToString<T[]>(transformedData);
+    } catch (e) {
+        console.error(e);
+        throw e; // Re-throw the error to handle it elsewhere
+    } finally {
+        await client.close();
+    }
 }
 
 export const findAggregate = async <T extends mongoDB.Document>(collection: string, aggregate: object[]) => {
