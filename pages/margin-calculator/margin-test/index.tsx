d98081b1dec9007d2fed83cbf51869afb32cbbf9
numberwang55 <cban0901@hotmail.com>
2024-07-22 12:23:44 +0100

    added onbuy to marginTestItem

--- pages/margin-calculator/margin-test/index.tsx
+++ pages/margin-calculator/margin-test/index.tsx
@@ -58,12 +58,13 @@ export function marginTestItem():MarginTestTemplate{
         tags: [],
         packaging: {editable: false, group: "", items: [], lock: false},
         postage: {id: "", modifier: "", price: 0},
-        prices: {amazon: 0, ebay: 0, magento: 0, purchase: 0, retail: 0, shop: 0, magentoSpecial: 0},
+        prices: {amazon: 0, ebay: 0, magento: 0, purchase: 0, retail: 0, shop: 0, magentoSpecial: 0, "onbuy v2": 0},
         marginData: {
             amazon: {primePostage: 0, primeProfit: 0, profitLastYear: 0, fees: 0, profit: 0, salesVAT: 0},
             ebay: {profitLastYear: 0, fees: 0, profit: 0, salesVAT: 0},
             magento: {profitLastYear: 0, fees: 0, profit: 0, salesVAT: 0},
             shop: {profitLastYear: 0, fees: 0, profit: 0, salesVAT: 0},
+            "onbuy v2": {profitLastYear: 0, fees: 0, profit: 0, salesVAT: 0},
             packaging: 0,
             postage: 0,
             totalProfitLastYear: 0
